570deb0fef9dbb175ae50bf20efb65fd953e3802
Zolika1351 <zolika1351@mail.com>
2023-01-01 01:15:36 +0100

    g_NeedUpdate

--- include/Draw.h
+++ include/Draw.h
@@ -6,13 +6,14 @@ auto& DrawLightFlares = *(bool*)0xA6C088;
 auto& EnableSky = *(bool*)0xA73358;
 auto& DrawSky = *(bool*)0xA7335C;
 auto& DrawSkyEnvMap = *(bool*)0xA73360;
+auto& DrawFEng = *(bool*)0xA5E20C;
 auto& MainSkyScale = *(float*)0xA732AC;
 auto& SkydomeModel = *(eModel*)0xB77910;
 auto& CarScaleMatrix = *(UMath::Matrix4*)0xB778D0;
 auto& LeftTireRotateZMatrix = *(UMath::Matrix4*)0xB75000;
 auto& LeftTireMirrorMatrix = *(UMath::Matrix4*)0xB74EC0;
 
-auto& bJustChangedGraphics = *(bool*)0xAB0B25;
+auto& g_NeedUpdate = *(bool*)0xAB0B25;
 auto& g_FSAALevel = *(int*)0xA6537C;
 
 void Render_EnableCrashVisualTreatment(bool on)
